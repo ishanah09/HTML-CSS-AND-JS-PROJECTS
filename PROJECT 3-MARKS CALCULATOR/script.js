c5f0d673474abd746f37791b43ba627309dd423e
ishanah09 <ishanahmad0906@gmail.com>
2024-12-23 18:12:33 +0530

    addibg 3rd project

---
+++ PROJECT 3-MARKS CALCULATOR/script.js
@@ -0,0 +1,19 @@
+let resultEl = document.querySelector("#result");
+let formEl = document.querySelector("#calculateForm");
+
+function calculate(event) {
+  event.preventDefault();
+
+  const formData = new FormData(formEl);
+  let data = {};
+
+  formData.forEach((value, key) => {
+    data[key] = +value;
+  });
+
+  const totalMarks =
+    data.math + data.science + data.hindi + data.english + data.computer;
+  let percentage = Math.round((totalMarks / 500) * 100);
+
+  resultEl.innerHTML = `You have got ${totalMarks} marks out of ${marks} and your percentage is ${percentage}%`;
+}
